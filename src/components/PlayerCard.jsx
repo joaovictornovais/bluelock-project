8ae3c467642cdf987b45280a37fe2a058db5d59d
joaovictor <joao.dipaay@gmail.com>
2024-01-31 23:18:49 -0300

    refactor: team players style

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,13 +1,15 @@
 const PlayerCard = ({ name, image, rank }) => {
   return (
-    <div className="flex flex-col gap-1 text-center justify-center items-center">
-      <img
-        src={image}
-        alt={name}
-        className="rounded-full w-32 h-32 object-cover"
-      />
+    <div className="flex flex-col gap-1 text-center justify-center items-center ">
+      <div className="overflow-hidden rounded-full">
+        <img
+          src={image}
+          alt={name}
+          className="rounded-full w-32 h-32 object-cover hover:scale-110 transition-all"
+        />
+      </div>
       <p className="font-semibold">{name}</p>
-      <span>Rank: {rank}</span>
+      <span className="text-sm">Rank: {rank}</span>
     </div>
   );
 };
--- src/screens/Team.jsx
+++ src/screens/Team.jsx
@@ -20,7 +20,7 @@ const Team = () => {
     <section>
       <Header />
       <div className="pt-[80px]">
-        <div className="max-w-7xl mx-auto p-4 flex flex-col gap-4">
+        <div className="max-w-7xl mx-auto p-4 flex flex-col gap-8">
           <div className="text-center">
             <h1 className="font-bold text-4xl">Time {team}</h1>
             <h2 className="font-medium">
@@ -28,7 +28,7 @@ const Team = () => {
               <span className="text-blue-700">{handleAverageRank()}</span>
             </h2>
           </div>
-          <div className="grid grid-cols-4 gap-6">
+          <div className="grid grid-cols-4 gap-8">
             {players.map((player) => (
               <PlayerCard
                 key={player.id}
