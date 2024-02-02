e034f530ef6d2589a48f5ff6e10fe8b151a406b7
João Victor Novais <joao.dipaay@gmail.com>
2024-02-02 11:19:21 -0300

    refactor: teams in center

--- src/screens/Teams.jsx
+++ src/screens/Teams.jsx
@@ -9,7 +9,7 @@ const Teams = () => {
     <main>
       <Header />
       <div className="pt-[80px]">
-        <div className="max-w-7xl mx-auto p-4">
+        <div className="max-w-7xl mx-auto p-4 flex justify-center">
           <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-4">
             {teams.map((team) => (
               <Link to={`/times/${team.name}`}>
